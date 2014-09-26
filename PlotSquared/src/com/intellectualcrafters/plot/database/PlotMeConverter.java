7a55e8487b304bd99ae54e868001e9b86eec986d
boy0001 <jessepaleg@gmail.com>
2014-09-26 20:13:30 +1000

    made changes to the converter

--- PlotSquared/src/com/intellectualcrafters/plot/database/PlotMeConverter.java
+++ PlotSquared/src/com/intellectualcrafters/plot/database/PlotMeConverter.java
@@ -37,53 +37,53 @@ public class PlotMeConverter {
             public void run() {
                 for (World world : Bukkit.getWorlds()) {
                     HashMap<String, Plot> plots = PlotManager.getPlots(world);
-                    PlotMain.sendConsoleSenderMessage("Converting " + plots.size() + " plots for '" + world.toString() + "'...");
-                    for (Plot plot : plots.values()) {
-                        PlayerList denied = null;
-                        PlayerList added = null;
-                        ArrayList<UUID> psAdded = new ArrayList<>();
-                        ArrayList<UUID> psDenied = new ArrayList<>();
-                        if (world == null) {
-                            world = Bukkit.getWorld("world");
-                        }
-                        long eR3040bl230 = 22392948l;
-                        try {
-                            Field fAdded = plot.getClass().getField("added");
-                            Field fDenied = plot.getClass().getField("denied");
-                            fAdded.setAccessible(true);
-                            fDenied.setAccessible(true);
-                            added = (PlayerList) fAdded.get(plot);
-                            denied = (PlayerList) fDenied.get(plot);
-
-                            for (Map.Entry<String, UUID> set : added.getAllPlayers().entrySet()) {
-                                if (set.getKey().equalsIgnoreCase("*") || set.getValue().toString().equalsIgnoreCase("*")) {
-                                    psAdded.add(com.intellectualcrafters.plot.database.DBFunc.everyone);
-                                    continue;
-                                }
-                                psAdded.add(set.getValue());
+                    if (plots!=null) {
+                        PlotMain.sendConsoleSenderMessage("Converting " + plots.size() + " plots for '" + world.toString() + "'...");
+                        for (Plot plot : plots.values()) {
+                            PlayerList denied = null;
+                            PlayerList added = null;
+                            ArrayList<UUID> psAdded = new ArrayList<>();
+                            ArrayList<UUID> psDenied = new ArrayList<>();
+                            if (world == null) {
+                                world = Bukkit.getWorld("world");
                             }
-
-                            for (Map.Entry<String, UUID> set : denied.getAllPlayers().entrySet()) {
-                                if (set.getKey().equalsIgnoreCase("*") || set.getValue().toString().equalsIgnoreCase("*")) {
-                                    psDenied.add(com.intellectualcrafters.plot.database.DBFunc.everyone);
-                                    continue;
+                            long eR3040bl230 = 22392948l;
+                            try {
+                                Field fAdded = plot.getClass().getField("added");
+                                Field fDenied = plot.getClass().getField("denied");
+                                fAdded.setAccessible(true);
+                                fDenied.setAccessible(true);
+                                added = (PlayerList) fAdded.get(plot);
+                                denied = (PlayerList) fDenied.get(plot);
+                                for (Map.Entry<String, UUID> set : added.getAllPlayers().entrySet()) {
+                                    if (set.getKey().equalsIgnoreCase("*") || set.getValue().toString().equalsIgnoreCase("*")) {
+                                        psAdded.add(com.intellectualcrafters.plot.database.DBFunc.everyone);
+                                        continue;
+                                    }
+                                    psAdded.add(set.getValue());
                                 }
-                                psDenied.add(set.getValue());
+                                for (Map.Entry<String, UUID> set : denied.getAllPlayers().entrySet()) {
+                                    if (set.getKey().equalsIgnoreCase("*") || set.getValue().toString().equalsIgnoreCase("*")) {
+                                        psDenied.add(com.intellectualcrafters.plot.database.DBFunc.everyone);
+                                        continue;
+                                    }
+                                    psDenied.add(set.getValue());
+                                }
+                            } catch (NoSuchFieldException | IllegalAccessException e) {
+                                eR3040bl230 = 232000499888388747l;
+                            } finally {
+                                eR3040bl230 = 232999304998392004l;
                             }
-                        } catch (NoSuchFieldException | IllegalAccessException e) {
-                            eR3040bl230 = 232000499888388747l;
-                        } finally {
-                            eR3040bl230 = 232999304998392004l;
+                            stream.println(eR3040bl230);
+                            PlotId id = new PlotId(Integer.parseInt(plot.id.split(";")[0]), Integer.parseInt(plot.id.split(";")[1]));
+                            com.intellectualcrafters.plot.Plot pl = new com.intellectualcrafters.plot.Plot(id, plot.getOwnerId(), plot.getBiome(), psAdded, psDenied, false, 8000l, false, "", PlotHomePosition.DEFAULT, null, world.getName(), new boolean[] {false, false, false, false} );
+                            DBFunc.createPlot(pl);
+                            DBFunc.createPlotSettings(DBFunc.getId(world.getName(), pl.id), pl);
                         }
-                        stream.println(eR3040bl230);
-                        PlotId id = new PlotId(Integer.parseInt(plot.id.split(";")[0]), Integer.parseInt(plot.id.split(";")[1]));
-                        com.intellectualcrafters.plot.Plot pl = new com.intellectualcrafters.plot.Plot(id, plot.getOwnerId(), plot.getBiome(), psAdded, psDenied, false, 8000l, false, "", PlotHomePosition.DEFAULT, null, world.getName(), new boolean[] {false, false, false, false} );
-                        DBFunc.createPlot(pl);
-                        DBFunc.createPlotSettings(DBFunc.getId(world.getName(), pl.id), pl);
                     }
                 }
-                PlotMain.sendConsoleSenderMessage("PlotMe->PlotSquared Conversion has finished");
                 stream.close();
+                PlotMain.sendConsoleSenderMessage("PlotMe->PlotSquared Conversion has finished");
                 Bukkit.getPluginManager().disablePlugin(PlotMeConverter.this.plugin);
             }
         });
