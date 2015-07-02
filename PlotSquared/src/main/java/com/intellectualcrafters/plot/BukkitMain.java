a81ea1726b18a10bf78ea9874f568aac6f1a3fc3
sauilitired <citymonstret@hotmail.com>
2015-07-02 20:27:15 +0200

    Fixes #321 (again)

--- PlotSquared/src/main/java/com/intellectualcrafters/plot/BukkitMain.java
+++ PlotSquared/src/main/java/com/intellectualcrafters/plot/BukkitMain.java
@@ -232,6 +232,7 @@ public class BukkitMain extends JavaPlugin implements Listener, IPlotMain {
         Player player = Bukkit.getPlayer(uuid);
         if (player != null && player.isOnline()) {
             MainUtil.sendMessage(BukkitUtil.getPlayer(player), c);
+            player.teleport(player.getWorld().getSpawnLocation());
         }
     }
 
