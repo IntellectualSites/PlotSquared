5e33c175ab0b3f1b910f78b93f0ec7aeda939567
manuelgu <manuelgu@users.noreply.github.com>
2015-12-17 18:11:50 +0100

    Typo in "ice-melt" flag
    The flag doesnt seem to be used anyways, so not much of a deal.

--- src/main/java/com/intellectualcrafters/plot/PS.java
+++ src/main/java/com/intellectualcrafters/plot/PS.java
@@ -1649,7 +1649,7 @@ public class PS {
         FlagManager.addFlag(new AbstractFlag("break", new FlagValue.PlotBlockListValue()));
         FlagManager.addFlag(new AbstractFlag("use", new FlagValue.PlotBlockListValue()));
         FlagManager.addFlag(new AbstractFlag("blocked-cmds", new FlagValue.StringListValue()));
-        FlagManager.addFlag(new AbstractFlag("ice-met", new FlagValue.BooleanValue()));
+        FlagManager.addFlag(new AbstractFlag("ice-melt", new FlagValue.BooleanValue()));
         FlagManager.addFlag(new AbstractFlag("gamemode") {
             
             @Override
