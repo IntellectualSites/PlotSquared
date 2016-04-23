1bce69d28865711dbb9645275b96535b16dd4754
MattBDev <mattbdev@outlook.com>
2016-04-22 23:54:53 -0400

    Merge flags to master

--- Core/src/main/java/com/intellectualcrafters/plot/commands/Download.java
+++ Core/src/main/java/com/intellectualcrafters/plot/commands/Download.java
@@ -41,7 +41,8 @@ public class Download extends SubCommand {
             MainUtil.sendMessage(plr, C.PLOT_UNOWNED);
             return false;
         }
-        if ((Settings.DOWNLOAD_REQUIRES_DONE && (FlagManager.getPlotFlagRaw(plot, "done") == null)) && !Permissions.hasPermission(plr, "plots.admin.command.download")) {
+        if ((Settings.DOWNLOAD_REQUIRES_DONE && (!plot.getFlag(Flags.DONE).isPresent())) && !Permissions
+                .hasPermission(plr, "plots.admin.command.download")) {
             MainUtil.sendMessage(plr, C.DONE_NOT_DONE);
             return false;
         }
