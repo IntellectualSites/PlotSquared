770b9be1607207412915d550161a71d27e88d4ea
Jesse Boyd <jessepaleg@gmail.com>
2016-07-25 09:23:20 +1000

    Minor uuid cache changes

--- Sponge/src/main/java/com/plotsquared/sponge/generator/SpongeAugmentedGenerator.java
+++ Sponge/src/main/java/com/plotsquared/sponge/generator/SpongeAugmentedGenerator.java
@@ -4,9 +4,12 @@ import com.flowpowered.math.vector.Vector3i;
 import com.intellectualcrafters.plot.generator.AugmentedUtils;
 import com.intellectualcrafters.plot.object.PlotBlock;
 import com.intellectualcrafters.plot.util.block.DelegateLocalBlockQueue;
+import com.plotsquared.sponge.SpongeMain;
 import com.plotsquared.sponge.util.SpongeUtil;
 import java.util.List;
 import org.spongepowered.api.block.BlockState;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.extent.ImmutableBiomeArea;
 import org.spongepowered.api.world.extent.MutableBlockVolume;
@@ -33,6 +36,8 @@ public class SpongeAugmentedGenerator implements GenerationPopulator {
         populators.add(generator);
         return generator;
     }
+
+    private Cause cause = Cause.of(NamedCause.of("PlotSquared", SpongeMain.THIS));
     
     @Override
     public void populate(World world, MutableBlockVolume terrain, ImmutableBiomeArea biome) {
@@ -44,7 +49,7 @@ public class SpongeAugmentedGenerator implements GenerationPopulator {
         AugmentedUtils.generate(world.getName(), cx, cz, new DelegateLocalBlockQueue(null) {
             @Override
             public boolean setBlock(int x, int y, int z, int id, int data) {
-                terrain.setBlock(bx + x, y, bz + z, SpongeUtil.getBlockState(id, data));
+                terrain.setBlock(bx + x, y, bz + z, SpongeUtil.getBlockState(id, data), cause);
                 return true;
             }
 
