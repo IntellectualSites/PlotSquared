dea0a452dffdeff61bf4985df45836c88d38b6b3
Jesse Boyd <jessepaleg@gmail.com>
2016-11-30 16:28:50 +1100

    Add fading 1.11 titles

--- Bukkit/src/main/java/com/plotsquared/bukkit/titles/DefaultTitle_110.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/titles/DefaultTitle_110.java
@@ -4,7 +4,7 @@ import com.intellectualcrafters.plot.object.PlotPlayer;
 import com.intellectualcrafters.plot.util.AbstractTitle;
 import com.plotsquared.bukkit.object.BukkitPlayer;
 
-public class DefaultTitle extends AbstractTitle {
+public class DefaultTitle_110 extends AbstractTitle {
 
     @Override
     public void sendTitle(PlotPlayer player, String head, String sub, int in, int delay, int out) {
@@ -12,7 +12,7 @@ public class DefaultTitle extends AbstractTitle {
             DefaultTitleManager title = new DefaultTitleManager(head, sub, in, delay, out);
             title.send(((BukkitPlayer) player).player);
         } catch (Exception ignored) {
-            AbstractTitle.TITLE_CLASS = new DefaultTitle_183();
+            AbstractTitle.TITLE_CLASS = new DefaultTitle_19();
             AbstractTitle.TITLE_CLASS.sendTitle(player, head, sub, in, delay, out);
         }
     }
--- Bukkit/src/main/java/com/plotsquared/bukkit/titles/DefaultTitle_111.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/titles/DefaultTitle_111.java
@@ -2,26 +2,20 @@ package com.plotsquared.bukkit.titles;
 
 import com.intellectualcrafters.plot.object.PlotPlayer;
 import com.intellectualcrafters.plot.util.AbstractTitle;
-import com.intellectualcrafters.plot.util.TaskManager;
 import com.plotsquared.bukkit.object.BukkitPlayer;
 import org.bukkit.entity.Player;
 
 @SuppressWarnings("deprecation")
-public class DefaultTitle_19 extends AbstractTitle {
+public class DefaultTitle_111 extends AbstractTitle {
 
     @Override
     public void sendTitle(PlotPlayer player, String head, String sub, int in, int delay, int out) {
         try {
             final Player playerObj = ((BukkitPlayer) player).player;
-            playerObj.sendTitle(head, sub);
-            TaskManager.runTaskLater(new Runnable() {
-                @Override
-                public void run() {
-                    playerObj.sendTitle("", "");
-                }
-            }, delay * 20);
+            TitleManager_1_11 title = new TitleManager_1_11(head, sub, in, delay, out);
+            title.send(playerObj);
         } catch (Throwable ignored) {
-            AbstractTitle.TITLE_CLASS = new DefaultTitle_183();
+            AbstractTitle.TITLE_CLASS = new DefaultTitle_110();
             AbstractTitle.TITLE_CLASS.sendTitle(player, head, sub, in, delay, out);
         }
     }
