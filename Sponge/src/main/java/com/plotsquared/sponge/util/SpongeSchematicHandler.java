2e7d95f5a6be26388e149abb305cd4d3a0de1b06
Jesse Boyd <jessepaleg@gmail.com>
2018-09-10 07:45:00 +1000

    Fix sponge schem save

--- Sponge/src/main/java/com/plotsquared/sponge/util/SpongeSchematicHandler.java
+++ Sponge/src/main/java/com/plotsquared/sponge/util/SpongeSchematicHandler.java
@@ -123,11 +123,11 @@ public class SpongeSchematicHandler extends SchematicHandler {
                                     ChunkLoc chunk = chunks.remove(0);
                                     int X = chunk.x;
                                     int Z = chunk.z;
-                                    int xxb = X << 4;
-                                    int zzb = Z << 4;
-                                    if (!worldObj.getChunk(xxb, 1, zzb).isPresent() && !worldObj.loadChunk(xxb, 1, zzb, false).isPresent()) {
+                                    if (!worldObj.getChunk(X, 0, Z).isPresent() && !worldObj.loadChunk(X, 0, Z, false).isPresent()) {
                                         continue;
                                     }
+                                    int xxb = X << 4;
+                                    int zzb = Z << 4;
                                     int xxt = xxb + 15;
                                     int zzt = zzb + 15;
                                     
