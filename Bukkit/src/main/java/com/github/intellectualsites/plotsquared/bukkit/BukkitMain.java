a4c3fb26c79f5f3e8fe35e47e8247877beb8a93b
dordsor21 <dordsor21@gmail.com>
2019-01-23 14:02:18 +0000

    Check for road or unowned plot

--- Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/BukkitMain.java
+++ Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/BukkitMain.java
@@ -431,8 +431,9 @@ public final class BukkitMain extends JavaPlugin implements Listener, IPlotMain 
                                 case FIREBALL:
                                 case DRAGON_FIREBALL:
                                 case DROPPED_ITEM:
-                                    if (Settings.Enabled_Components.KILL_ROAD_ITEMS
-                                        && BukkitUtil.getPlot(entity.getLocation()) == null) {
+                                    if (Settings.Enabled_Components.KILL_ROAD_ITEMS || plotArea
+                                        .getOwnedPlotAbs(
+                                            BukkitUtil.getLocation(entity.getLocation())) == null) {
                                         entity.remove();
                                     }
                                     // dropped item
