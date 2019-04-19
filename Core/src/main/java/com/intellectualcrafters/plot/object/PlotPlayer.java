28148ade4bc8f0b0a4bb1c9c7b7b920b88305692
Jesse Boyd <jessepaleg@gmail.com>
2019-04-19 12:54:57 +1000

    Fix teleportOnLogin error

--- Core/src/main/java/com/intellectualcrafters/plot/object/PlotPlayer.java
+++ Core/src/main/java/com/intellectualcrafters/plot/object/PlotPlayer.java
@@ -554,8 +554,10 @@ public abstract class PlotPlayer implements CommandCaller, OfflinePlotPlayer {
                                                                 @Override
                                                                 public void run() {
                                                                     if (getMeta("teleportOnLogin", true)) {
-                                                                        teleport(loc);
-                                                                        sendMessage(C.TELEPORTED_TO_PLOT.f() + " (quitLoc-unloaded) (" + plotX + "," + plotZ + ")");
+                                                                        if (plot.isLoaded()) {
+                                                                            teleport(loc);
+                                                                            sendMessage(C.TELEPORTED_TO_PLOT.f() + " (quitLoc-unloaded) (" + plotX + "," + plotZ + ")");
+                                                                        }
                                                                     }
                                                                 }
                                                             });
