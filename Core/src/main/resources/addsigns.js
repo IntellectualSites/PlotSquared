bd0dab7d32aca5bafb848c083bff1378888f7d63
Leomixer17 <leonardo.digianfelicestornelli@gmail.com>
2019-07-06 01:16:45 +0200

    Update addsigns.js

--- Core/src/main/resources/addsigns.js
+++ Core/src/main/resources/addsigns.js
@@ -4,7 +4,7 @@ This script will fix all signs in the world.
 var plots = PS.sortPlotsByTemp(PS.getPlots());
 for (var i = 0; i < plots.size(); i++) {
     var plot = plots.get(i);
-    if (!plot.isMerged() || !plot.getMerged(0)) {
+    if (plot.isBasePlot()) {
         plot.setSign();
         PS.class.static.log('&cSetting sign for: ' + plot);
     }
