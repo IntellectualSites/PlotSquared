c6a420626631b7f85634ba1235bd77cbbf833dcb
NotMyFault <mc.cache@web.de>
2019-08-17 17:06:17 +0200

    Fix Typo

--- Core/src/main/java/com/github/intellectualsites/plotsquared/plot/generator/HybridUtils.java
+++ Core/src/main/java/com/github/intellectualsites/plotsquared/plot/generator/HybridUtils.java
@@ -183,7 +183,7 @@ public abstract class HybridUtils {
                                     ChunkLoc loc = iterator.next();
                                     iterator.remove();
                                     PlotSquared.debug("&3Updating .mcr: " + loc.x + ", " + loc.z
-                                        + " (aprrox 1024 chunks)");
+                                        + " (approx 1024 chunks)");
                                     PlotSquared.debug(" - Remaining: " + regions.size());
                                     chunks.addAll(getChunks(loc));
                                     System.gc();
