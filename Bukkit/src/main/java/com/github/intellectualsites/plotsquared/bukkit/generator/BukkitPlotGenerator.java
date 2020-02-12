99a53cb005f058b931f49ea5443f3fdd9fd9e3c4
MattBDev <4009945+MattBDev@users.noreply.github.com>
2020-02-12 18:57:42 -0500

    DoogiePC doesn't like trees

--- Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/generator/BukkitPlotGenerator.java
+++ Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/generator/BukkitPlotGenerator.java
@@ -191,7 +191,7 @@ public class BukkitPlotGenerator extends ChunkGenerator
     }
 
     public boolean shouldGenerateDecorations() {
-        return true;
+        return false;
     }
 
     public boolean isParallelCapable() {
