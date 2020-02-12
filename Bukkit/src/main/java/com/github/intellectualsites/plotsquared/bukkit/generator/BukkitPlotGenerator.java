d7af409d0179d4989a322e30758cc3f49de844df
MattBDev <4009945+MattBDev@users.noreply.github.com>
2020-02-12 18:21:23 -0500

    Test another workaround

--- Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/generator/BukkitPlotGenerator.java
+++ Bukkit/src/main/java/com/github/intellectualsites/plotsquared/bukkit/generator/BukkitPlotGenerator.java
@@ -191,7 +191,7 @@ public class BukkitPlotGenerator extends ChunkGenerator
     }
 
     public boolean shouldGenerateDecorations() {
-        return false;
+        return true;
     }
 
     public boolean isParallelCapable() {
