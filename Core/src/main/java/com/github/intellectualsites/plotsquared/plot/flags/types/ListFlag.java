e9c69dc80c96dd5b560ca3f4cf43dd91c27db4de
Alexander Söderberg <sauilitired@gmail.com>
2020-02-16 14:49:47 +0100

    Make ListFlag lists immutable

--- Core/src/main/java/com/github/intellectualsites/plotsquared/plot/flags/types/ListFlag.java
+++ Core/src/main/java/com/github/intellectualsites/plotsquared/plot/flags/types/ListFlag.java
@@ -7,12 +7,13 @@ import com.github.intellectualsites.plotsquared.plot.util.StringMan;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public abstract class ListFlag<V, F extends PlotFlag<List<V>, F>> extends PlotFlag<List<V>, F> {
 
     public ListFlag(final List<V> valueList, final Captions category, final Caption description) {
-        super(valueList, category, description);
+        super(Collections.unmodifiableList(valueList), category, description);
     }
 
     @Override public F merge(@NotNull List<V> newValue) {
