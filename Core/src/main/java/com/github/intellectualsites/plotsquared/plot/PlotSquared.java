6d779dad050cea295a3828985da26e7014460079
NotMyFault <mc.cache@web.de>
2020-02-20 00:39:32 +0100

    Remove unused code
    Or at least I made it unused lul

--- Core/src/main/java/com/github/intellectualsites/plotsquared/plot/PlotSquared.java
+++ Core/src/main/java/com/github/intellectualsites/plotsquared/plot/PlotSquared.java
@@ -60,18 +60,14 @@ import lombok.NonNull;
 import lombok.Setter;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
 import java.sql.SQLException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -79,7 +75,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -1606,18 +1601,6 @@ import java.util.zip.ZipInputStream;
             }
         }
         Settings.load(configFile);
-        //Sets the version information for the settings.yml file
-        try (InputStream stream = getClass().getResourceAsStream("/plugin.properties")) {
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
-                String versionString = br.readLine();
-                String commitString = br.readLine();
-                String dateString = br.readLine();
-                this.version = PlotVersion.tryParse(versionString, commitString, dateString);
-                System.out.println("Version is " + this.version);
-            }
-        } catch (IOException throwable) {
-            throwable.printStackTrace();
-        }
         Settings.save(configFile);
         config = YamlConfiguration.loadConfiguration(configFile);
     }
