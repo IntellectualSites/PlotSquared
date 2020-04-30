894d673c902a0e7680ef9d148a8f43996faefb93
N0tMyFaultOG <mc.cache@web.de>
2020-04-30 11:32:50 +0200

    Fix tests

--- Core/src/test/java/com/plotsquared/core/plot/PlotVersionTest.java
+++ Core/src/test/java/com/plotsquared/core/plot/PlotVersionTest.java
@@ -33,7 +33,7 @@ public class PlotVersionTest {
     @Test public void tryParse() {
         //These are all random values chosen to form the test class.
         PlotVersion version = new PlotVersion("4.340", "f06903f", "19.08.05");
-        System.out.println(version.build);
+        System.out.println(version.versionString);
 
     }
 }
