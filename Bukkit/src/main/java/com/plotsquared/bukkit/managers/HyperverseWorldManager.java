31edffcb639940f33e10a5a4190594baac97cf15
Alexander Söderberg <sauilitired@gmail.com>
2020-05-15 18:41:57 +0200

    Begin working on a platform specific world management system
    This should make world management a fair bit cleaner

---
+++ Bukkit/src/main/java/com/plotsquared/bukkit/managers/HyperverseWorldManager.java
@@ -0,0 +1,66 @@
+/*
+ *       _____  _       _    _____                                _
+ *      |  __ \| |     | |  / ____|                              | |
+ *      | |__) | | ___ | |_| (___   __ _ _   _  __ _ _ __ ___  __| |
+ *      |  ___/| |/ _ \| __|\___ \ / _` | | | |/ _` | '__/ _ \/ _` |
+ *      | |    | | (_) | |_ ____) | (_| | |_| | (_| | | |  __/ (_| |
+ *      |_|    |_|\___/ \__|_____/ \__, |\__,_|\__,_|_|  \___|\__,_|
+ *                                    | |
+ *                                    |_|
+ *            PlotSquared plot management system for Minecraft
+ *                  Copyright (C) 2020 IntellectualSites
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.plotsquared.bukkit.managers;
+
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import se.hyperver.hyperverse.Hyperverse;
+import se.hyperver.hyperverse.world.WorldConfiguration;
+import se.hyperver.hyperverse.world.WorldConfigurationBuilder;
+import se.hyperver.hyperverse.world.WorldFeatures;
+import se.hyperver.hyperverse.world.WorldType;
+
+/**
+ * Hyperverse specific manager that creates worlds
+ * using Hyperverse's API
+ */
+public class HyperverseWorldManager extends BukkitWorldManager {
+
+    @Override @Nullable
+    public World handleWorldCreation(@NotNull String worldName, @Nullable String generator) {
+        // First let Bukkit register the world
+        this.setGenerator(worldName, generator);
+        // Create the world
+        final WorldConfigurationBuilder worldConfigurationBuilder = WorldConfiguration.builder()
+            .setName(worldName).setType(WorldType.OVER_WORLD);
+        if (generator != null) {
+            worldConfigurationBuilder.setGenerator(generator).setWorldFeatures(WorldFeatures.FLATLAND);
+        }
+        try {
+            return Hyperverse.getApi().createWorld(worldConfigurationBuilder.createWorldConfiguration())
+                .getBukkitWorld();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override public String getName() {
+        return "bukkit-hyperverse";
+    }
+
+}
---
+++ Bukkit/src/main/java/com/plotsquared/bukkit/managers/MultiverseWorldManager.java
@@ -0,0 +1,57 @@
+/*
+ *       _____  _       _    _____                                _
+ *      |  __ \| |     | |  / ____|                              | |
+ *      | |__) | | ___ | |_| (___   __ _ _   _  __ _ _ __ ___  __| |
+ *      |  ___/| |/ _ \| __|\___ \ / _` | | | |/ _` | '__/ _ \/ _` |
+ *      | |    | | (_) | |_ ____) | (_| | |_| | (_| | | |  __/ (_| |
+ *      |_|    |_|\___/ \__|_____/ \__, |\__,_|\__,_|_|  \___|\__,_|
+ *                                    | |
+ *                                    |_|
+ *            PlotSquared plot management system for Minecraft
+ *                  Copyright (C) 2020 IntellectualSites
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.plotsquared.bukkit.managers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Multiverse specific manager that informs Multiverse of
+ * world creation by executing a console command
+ */
+public class MultiverseWorldManager extends BukkitWorldManager {
+
+    @Override @Nullable
+    public World handleWorldCreation(@NotNull final String worldName, @Nullable final String generator) {
+        // First let Bukkit register the world
+        this.setGenerator(worldName, generator);
+        // Then we send the console command
+        final StringBuilder commandBuilder = new StringBuilder("mv create ")
+            .append(worldName).append(" normal");
+        if (generator != null && !generator.isEmpty()) {
+            commandBuilder.append(" -g ").append(generator);
+        }
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), commandBuilder.toString());
+        return Bukkit.getWorld(worldName);
+    }
+
+    @Override public String getName() {
+        return "bukkit-multiverse";
+    }
+
+}
