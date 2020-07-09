61de18190f084402a37fc67d8ca85c2ee86aec52
Traks <traksag@protonmail.com>
2020-07-09 02:11:51 +0200

    More dispense blocking on roads

--- Bukkit/src/main/java/com/plotsquared/bukkit/listener/PlayerEvents.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/listener/PlayerEvents.java
@@ -1618,6 +1618,35 @@ public class PlayerEvents extends PlotListener implements Listener {
     public void onBlockDispense(BlockDispenseEvent event) {
         Material type = event.getItem().getType();
         switch (type) {
+            case SHULKER_BOX:
+            case WHITE_SHULKER_BOX:
+            case ORANGE_SHULKER_BOX:
+            case MAGENTA_SHULKER_BOX:
+            case LIGHT_BLUE_SHULKER_BOX:
+            case YELLOW_SHULKER_BOX:
+            case LIME_SHULKER_BOX:
+            case PINK_SHULKER_BOX:
+            case GRAY_SHULKER_BOX:
+            case LIGHT_GRAY_SHULKER_BOX:
+            case CYAN_SHULKER_BOX:
+            case PURPLE_SHULKER_BOX:
+            case BLUE_SHULKER_BOX:
+            case BROWN_SHULKER_BOX:
+            case GREEN_SHULKER_BOX:
+            case RED_SHULKER_BOX:
+            case BLACK_SHULKER_BOX:
+            case CARVED_PUMPKIN:
+            case WITHER_SKELETON_SKULL:
+            case FLINT_AND_STEEL:
+            case BONE_MEAL:
+            case SHEARS:
+            case GLASS_BOTTLE:
+            case GLOWSTONE:
+            case COD_BUCKET:
+            case PUFFERFISH_BUCKET:
+            case SALMON_BUCKET:
+            case TROPICAL_FISH_BUCKET:
+            case BUCKET:
             case WATER_BUCKET:
             case LAVA_BUCKET: {
                 if (event.getBlock().getType() == Material.DROPPER) {
