b5fecf56a0b7b02a83a15ad2b224c8fe57263446
Mariell Hoversholm <proximyst@proximyst.com>
2020-10-08 13:27:23 +0200

    It runs! And it's small!

--- Bukkit/build.gradle.kts
+++ Bukkit/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     // Plugins
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.0-SNAPSHOT") {
         exclude(group = "org.bukkit")
+        exclude(group = "org.spigotmc")
     }
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude(group = "org.bukkit")
@@ -60,61 +61,36 @@ dependencies {
     compileOnly("be.maximvdw:MVdWPlaceholderAPI:3.1.1") { isTransitive = false }
 
     // Other libraries
-    compileOnly("com.sk89q:squirrelid:1.0.0-SNAPSHOT") { isTransitive = false }
+    implementation("com.sk89q:squirrelid:1.0.0-SNAPSHOT") { isTransitive = false }
 
     // Adventure
     implementation("net.kyori:adventure-platform-bukkit:4.0.0-SNAPSHOT")
 }
 
-tasks.named<Copy>("processResources") {
+tasks.processResources {
     filesMatching("plugin.yml") {
         expand("version" to project.version)
     }
 }
 
-// TODO: Get this to be not ugly...
 tasks.named<ShadowJar>("shadowJar") {
     dependencies {
-        include(dependency(":PlotSquared-Core"))
-        include(dependency("io.papermc:paperlib"))
-        include(dependency("net.kyori:adventure-platform-bukkit"))
-        include(dependency("net.kyori:adventure-text-minimessage"))
-        include(dependency("net.kyori:adventure-text-serializer-bungeecord"))
-        include(dependency("net.kyori:adventure-text-serializer-legacy"))
-        include(dependency("net.kyori:adventure-text-serializer-gson"))
-        include(dependency("net.kyori:adventure-api"))
-        include(dependency("net.kyori:adventure-platform-api"))
-        include(dependency("net.kyori:adventure-platform-common"))
-        include(dependency("net.kyori:adventure-platform-viaversion"))
-        include(dependency("net.kyori:adventure-nbt"))
-        include(dependency("net.kyori:examination-api"))
-        include(dependency("net.kyori:examination-string"))
-        include(dependency("org.bstats:bstats-bukkit"))
-        include(dependency("org.khelekore:prtree"))
-        include(dependency("com.sk89q:squirrelid"))
-        include(dependency("com.google.inject:guice"))
-        include(dependency("com.google.inject.extensions:guice-assistedinject"))
-        include(dependency("javax.annotation:javax-annotation-api"))
-        include(dependency("org.apache.logging.log4j:log4j-slf4j-impl"))
-        include(dependency("org.slf4j:slf4j-api"))
-        include(dependency("javax.inject:javax.inject"))
-        include(dependency("aopalliance:aopalliance"))
-        include(dependency("com.intellectualsites:Pipeline"))
-
-        relocate("net.kyori.adventure", "com.plotsquared.core.configuration.adventure")
-        relocate("net.kyori.examination", "com.plotsquared.core.configuration.examination")
-        relocate("io.papermc.lib", "com.plotsquared.bukkit.paperlib")
-        relocate("org.bstats", "com.plotsquared.metrics")
-        relocate("com.sk89q.squirrelid", "com.plotsquared.squirrelid")
-        relocate("org.khelekore.prtree", "com.plotsquared.prtree")
-        relocate("org.apache.logging.slf4j", "com.plotsquared.logging.apache")
-        relocate("org.slf4j", "com.plotsquared.logging.slf4j")
-        relocate("com.google.inject", "com.plotsquared.google")
-        relocate("javax.inject", "com.plotsquared.core.inject.javax")
-        relocate("org.aopalliance", "com.plotsquared.core.aopalliance")
-        relocate("com.intellectualsites.services", "com.plotsquared.core.services")
-        relocate("org.json", "com.plotsquared.json") {
-            exclude("org/json/simple/**")
-        }
+        exclude(dependency("org.checkerframework:"))
     }
+
+    relocate("net.kyori.adventure", "com.plotsquared.core.configuration.adventure")
+    relocate("net.kyori.examination", "com.plotsquared.core.configuration.examination")
+    relocate("io.papermc.lib", "com.plotsquared.bukkit.paperlib")
+    relocate("org.bstats", "com.plotsquared.metrics")
+    relocate("com.sk89q.squirrelid", "com.plotsquared.squirrelid")
+    relocate("org.khelekore.prtree", "com.plotsquared.prtree")
+    relocate("org.apache.logging.slf4j", "com.plotsquared.logging.apache")
+    relocate("org.slf4j", "com.plotsquared.logging.slf4j")
+    relocate("com.google.inject", "com.plotsquared.google")
+    relocate("javax.inject", "com.plotsquared.core.inject.javax")
+    relocate("org.aopalliance", "com.plotsquared.core.aopalliance")
+    relocate("com.intellectualsites.services", "com.plotsquared.core.services")
+
+    // Get rid of all the libs which are 100% unused.
+    minimize()
 }
