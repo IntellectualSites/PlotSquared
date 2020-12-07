714837dd009e6da9717e6749ca6f980afd53f41a
N0tMyFaultOG <mc.cache@web.de>
2020-12-07 23:05:25 +0100

    Add charts for complex components

--- Bukkit/src/main/java/com/plotsquared/bukkit/BukkitPlatform.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/BukkitPlatform.java
@@ -74,6 +74,7 @@ import com.plotsquared.core.configuration.ConfigurationNode;
 import com.plotsquared.core.configuration.ConfigurationSection;
 import com.plotsquared.core.configuration.ConfigurationUtil;
 import com.plotsquared.core.configuration.Settings;
+import com.plotsquared.core.configuration.Storage;
 import com.plotsquared.core.configuration.caption.ChatFormatter;
 import com.plotsquared.core.configuration.file.YamlConfiguration;
 import com.plotsquared.core.database.DBFunc;
@@ -983,6 +984,10 @@ public final class BukkitPlatform extends JavaPlugin implements Listener, PlotPl
             return map;
         }));
         metrics.addCustomChart(new Metrics.SimplePie("premium", () -> PremiumVerification.isPremium() ? "Premium" : "Non-Premium"));
+        metrics.addCustomChart(new Metrics.SimplePie("worlds", () -> Settings.Enabled_Components.WORLDS ? "true" : "false"));
+        metrics.addCustomChart(new Metrics.SimplePie("economy", () -> Settings.Enabled_Components.ECONOMY ? "true" : "false"));
+        metrics.addCustomChart(new Metrics.SimplePie("plot_expiry", () -> Settings.Enabled_Components.PLOT_EXPIRY ? "true" : "false"));
+        metrics.addCustomChart(new Metrics.SimplePie("mysql", () -> Storage.MySQL.USE ? "true" : "false"));
         metrics.addCustomChart(new Metrics.SimplePie("worldedit_implementation",
             () -> Bukkit.getPluginManager().getPlugin("FastAsyncWorldEdit") != null ? "FastAsyncWorldEdit" : "WorldEdit"));
     }
