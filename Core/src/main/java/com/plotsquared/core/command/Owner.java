e8642df674a9174a2cb51cd706a0b41ee6cf6d18
N0tMyFaultOG <mc.cache@web.de>
2021-01-01 00:19:45 +0100

    Update licenses to 2021
    Happy new year :fireworks:

--- Core/src/main/java/com/plotsquared/core/command/Owner.java
+++ Core/src/main/java/com/plotsquared/core/command/Owner.java
@@ -8,7 +8,7 @@
  *                                    | |
  *                                    |_|
  *            PlotSquared plot management system for Minecraft
- *                  Copyright (C) 2020 IntellectualSites
+ *                  Copyright (C) 2021 IntellectualSites
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
