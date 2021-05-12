ca7ac71e76c92bda0bdf3d88fe4febcc8e1cfdbd
NotMyFault <mc.cache@web.de>
2021-05-12 03:00:34 +0200

    Update gradle and shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     java
     `java-library`
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
     id("org.cadixdev.licenser") version "0.5.1"
     id("org.ajoberstar.grgit") version "4.1.0"
 
