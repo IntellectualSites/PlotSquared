30ed6a158734dfe966f626b2bb814424bcc5fb79
IPat <ThisIsIPat@users.noreply.github.com>
2021-05-19 06:11:50 +0200

    Actually prevent purchase of plots if they can't afford it (whoops)

--- Core/src/main/java/com/plotsquared/core/command/Auto.java
+++ Core/src/main/java/com/plotsquared/core/command/Auto.java
@@ -322,6 +322,7 @@ public class Auto extends SubCommand {
                             Template.of("money", this.econHandler.format(cost)),
                             Template.of("balance", this.econHandler.format(this.econHandler.getMoney(player)))
                     );
+                    return false;
                 }
                 this.econHandler.withdrawMoney(player, cost);
                 player.sendMessage(
