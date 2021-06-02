bc3479f865abc9d1c2dcd4583e44c81cfe77dbb4
NotMyFault <mc.cache@web.de>
2021-06-02 23:22:42 +0200

    Address changes requested

--- Bukkit/build.gradle.kts
+++ Bukkit/build.gradle.kts
@@ -105,6 +105,11 @@ tasks.named<ShadowJar>("shadowJar") {
 tasks {
     withType<Javadoc> {
         val opt = options as StandardJavadocDocletOptions
-        opt.links("https://papermc.io/javadocs/paper/1.16")
+        opt.links("https://papermc.io/javadocs/paper/1.16/")
+        opt.links("https://docs.enginehub.org/javadoc/com.sk89q.worldedit/worldedit-core/7.2.5/")
+        opt.links("https://docs.enginehub.org/javadoc/com.sk89q.worldedit/worldedit-bukkit/7.2.5/")
+        opt.links("https://jd.adventure.kyori.net/api/4.7.0/")
+        opt.links("https://google.github.io/guice/api-docs/5.0.1/javadoc/")
+        opt.links("https://checkerframework.org/api/")
     }
 }
