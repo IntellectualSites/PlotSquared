1348484c1f342c0326369cc4e08da67dc84db1cc
Pierre Maurice Schwang <mail@pschwang.eu>
2021-10-01 22:35:49 +0200

    Respect tab-complete aliases (Fixes #3266)

--- Bukkit/src/main/java/com/plotsquared/bukkit/BukkitCommand.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/BukkitCommand.java
@@ -26,7 +26,9 @@
 package com.plotsquared.bukkit;
 
 import com.plotsquared.bukkit.util.BukkitUtil;
+import com.plotsquared.core.PlotSquared;
 import com.plotsquared.core.command.MainCommand;
+import com.plotsquared.core.configuration.Settings;
 import com.plotsquared.core.player.ConsolePlayer;
 import com.plotsquared.core.player.PlotPlayer;
 import org.bukkit.command.Command;
@@ -42,6 +44,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class BukkitCommand implements CommandExecutor, TabCompleter {
 
@@ -63,7 +66,7 @@ public class BukkitCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(
-            CommandSender commandSender, Command command, String s,
+            CommandSender commandSender, Command command, String label,
             String[] args
     ) {
         if (!(commandSender instanceof Player)) {
@@ -73,8 +76,11 @@ public class BukkitCommand implements CommandExecutor, TabCompleter {
         if (args.length == 0) {
             return Collections.singletonList("plots");
         }
+        if (!Settings.Enabled_Components.TAB_COMPLETED_ALIASES.contains(label.toLowerCase(Locale.ENGLISH))) {
+            return List.of();
+        }
         Collection<com.plotsquared.core.command.Command> objects =
-                MainCommand.getInstance().tab(player, args, s.endsWith(" "));
+                MainCommand.getInstance().tab(player, args, label.endsWith(" "));
         if (objects == null) {
             return null;
         }
