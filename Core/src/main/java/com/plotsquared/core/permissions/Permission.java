be85708e5ace8aaa911dd9a35b6f62bf2b65d8ee
Jordan <dordsor21@gmail.com>
2021-10-28 09:13:36 +0100

    feat: Add border bypass permission (#3302)
    * Add border bypass permission
     - Fixes #3297
    
    * Add permission to plots.admin permpack
    
    * Implement messages on leaving/entering border area
    
    * Rename to border.denied and make border msg red
    
    * change too le/ge as tthey are likely to be equal to border when hit.
     - Since we check for being across the border beforehand, it will hopefully not be spammed too much?
     - If it's spammed then either we use meta to set if a player's left the border, or we just remove the come-back-in altogether because it's a little looong

--- Core/src/main/java/com/plotsquared/core/permissions/Permission.java
+++ Core/src/main/java/com/plotsquared/core/permissions/Permission.java
@@ -204,7 +204,8 @@ public enum Permission {
     PERMISSION_ADMIN_DEBUG_OTHER("plots.admin.debug.other"),
     PERMISSION_RATE("plots.rate"),
     PERMISSION_ADMIN_FLIGHT("plots.admin.flight"),
-    PERMISSION_ADMIN_COMPONENTS_OTHER("plots.admin.component.other");
+    PERMISSION_ADMIN_COMPONENTS_OTHER("plots.admin.component.other"),
+    PERMISSION_ADMIN_BYPASS_BORDER("plots.admin.border.bypass");
     //</editor-fold>
 
     private final String text;
