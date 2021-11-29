184f304e7e0b45ad8706fcc2ba63e4dee248b2c1
Pierre Maurice Schwang <mail@pschwang.eu>
2021-11-29 19:21:59 +0100

    fix licenser issue

--- Core/src/main/java/com/plotsquared/core/events/post/package-info.java
+++ Core/src/main/java/com/plotsquared/core/events/post/package-info.java
@@ -23,8 +23,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-
 /**
  * Contains events which are fired after specific actions are completed.
  * <br>
