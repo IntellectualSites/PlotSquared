c446f66393bf417c78cd108512804cff04d2c32f
zombachu <zombachu@gmail.com>
2021-12-16 13:57:00 -0500

    docs: Add back deprecated projectile permissions

--- Core/src/main/java/com/plotsquared/core/permissions/Permission.java
+++ Core/src/main/java/com/plotsquared/core/permissions/Permission.java
@@ -37,6 +37,10 @@ public enum Permission {
     PERMISSION_STAR("*"),
     PERMISSION_ADMIN("plots.admin"),
     PERMISSION_ADMIN_AREA_SUDO("plots.admin.area.sudo"),
+    @Deprecated(forRemoval = true)
+    PERMISSION_PROJECTILE_UNOWNED("plots.projectile.unowned"),
+    @Deprecated(forRemoval = true)
+    PERMISSION_PROJECTILE_OTHER("plots.projectile.other"),
     PERMISSION_ADMIN_INTERACT_BLOCKED_CMDS("plots.admin.interact.blockedcommands"),
     PERMISSION_WORLDEDIT_BYPASS("plots.worldedit.bypass"),
     PERMISSION_PLOT_TOGGLE_TITLES("plots.toggle.titles"),
