74ae227a2b1f0c6621809e241eeeadd66abe6f83
dordsor21 <dordsor21@gmail.com>
2021-12-30 14:56:19 +0000

    Account for mutability of plot objects when sending move/swap success messages Fixes #3337

--- Core/src/main/java/com/plotsquared/core/command/Move.java
+++ Core/src/main/java/com/plotsquared/core/command/Move.java
@@ -105,13 +105,17 @@ public class Move extends SubCommand {
             return CompletableFuture.completedFuture(false);
         }
 
+        // Set strings here as the plot objects are mutable (the PlotID changes after being moved).
+        String p1 = plot1.toString();
+        String p2 = plot2.toString();
+
         return plot1.getPlotModificationManager().move(plot2, player, () -> {
         }, false).thenApply(result -> {
             if (result) {
                 player.sendMessage(
                         TranslatableCaption.of("move.move_success"),
-                        Template.of("origin", plot1.toString()),
-                        Template.of("target", plot2.toString())
+                        Template.of("origin", p1),
+                        Template.of("target", p2)
                 );
                 return true;
             } else {
