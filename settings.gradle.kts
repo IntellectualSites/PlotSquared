954c813cefd934fc164908b0218d39c47e3ab4da
NotMyFault <mc.cache@web.de>
2022-02-27 18:48:37 +0100

    build: Update gradle wrapper properly

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,4 @@ include("Core", "Bukkit")
 project(":Core").name = "PlotSquared-Core"
 project(":Bukkit").name = "PlotSquared-Bukkit"
 
-enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
