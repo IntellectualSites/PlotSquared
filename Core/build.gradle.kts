565838ad43c3bc975a78ea957a880d1cde58e3b2
Alexander Brandes <mc.cache@web.de>
2022-06-13 11:36:46 +0200

    [ci skip] chore: Update renovate excludes (Closes #3678)

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -59,7 +59,7 @@ tasks {
     withType<Javadoc> {
         val opt = options as StandardJavadocDocletOptions
         opt.links("https://docs.enginehub.org/javadoc/com.sk89q.worldedit/worldedit-core/" + libs.worldeditCore.get().versionConstraint.toString())
-        opt.links("https://jd.adventure.kyori.net/api/" + libs.adventure.get().versionConstraint.toString())
+        opt.links("https://jd.adventure.kyori.net/api/4.9.3/")
         opt.links("https://google.github.io/guice/api-docs/" + libs.guice.get().versionConstraint.toString() + "/javadoc/")
         opt.links("https://checkerframework.org/api/")
     }
