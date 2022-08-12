f2a50741356bdf2e850f4ec4711f4a039e2e7e3a
Pierre Maurice Schwang <mail@pschwang.eu>
2022-08-12 20:47:16 +0200

    chore: license headers and documentation

--- Core/src/main/java/com/plotsquared/core/events/PlotMoveEvent.java
+++ Core/src/main/java/com/plotsquared/core/events/PlotMoveEvent.java
@@ -1,3 +1,21 @@
+/*
+ * PlotSquared, a land and world management plugin for Minecraft.
+ * Copyright (C) IntellectualSites <https://intellectualsites.com>
+ * Copyright (C) IntellectualSites team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.plotsquared.core.events;
 
 import com.plotsquared.core.player.PlotPlayer;
@@ -28,9 +46,14 @@ public class PlotMoveEvent extends PlotPlayerEvent implements CancellablePlotEve
         this.destination = destination;
     }
 
+    /**
+     * @return The destination for the plot to be moved to.
+     * @since TODO
+     */
     public Plot destination() {
         return destination;
     }
+
 
     @Override
     public Result getEventResult() {
