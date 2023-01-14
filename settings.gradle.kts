9295bbfdaeefefd6d358b4b4a72b18e8f3011f8e
Alexander Brandes <mc.cache@web.de>
2023-01-14 17:18:34 +0100

    Separate workflows between PRs and pushes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 rootProject.name = "PlotSquared"
 
 include("Core", "Bukkit")
