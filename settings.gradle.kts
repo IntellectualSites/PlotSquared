0edf11975e7682ec22c56f6bb617887776b1a2ec
Alexander Brandes <mc.cache@web.de>
2023-01-14 17:28:48 +0100

    Fixup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,3 @@
-plugins {
-    id("com.gradle.enterprise") version("3.9")
-}
-
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
-
 rootProject.name = "PlotSquared"
 
 include("Core", "Bukkit")
