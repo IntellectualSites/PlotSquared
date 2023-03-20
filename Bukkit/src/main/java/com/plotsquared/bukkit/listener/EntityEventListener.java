adc655b706dbaeaf1a0ecec9a9f5f1ea2666d397
ByteExceptionM <git@byteexception.eu>
2023-03-20 10:30:29 +0100

    fix: Fix bees leaving their beehives with disabled natural_mob_spawning
    Signed-off-by: ByteExceptionM <git@byteexception.eu>

--- Bukkit/src/main/java/com/plotsquared/bukkit/listener/EntityEventListener.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/listener/EntityEventListener.java
@@ -152,7 +152,7 @@ public class EntityEventListener implements Listener {
                 }
             }
             case "REINFORCEMENTS", "NATURAL", "MOUNT", "PATROL", "RAID", "SHEARED", "SILVERFISH_BLOCK", "ENDER_PEARL",
-                    "TRAP", "VILLAGE_DEFENSE", "VILLAGE_INVASION", "BEEHIVE", "CHUNK_GEN" -> {
+                    "TRAP", "VILLAGE_DEFENSE", "VILLAGE_INVASION", "CHUNK_GEN" -> {
                 if (!area.isMobSpawning()) {
                     event.setCancelled(true);
                     return;
