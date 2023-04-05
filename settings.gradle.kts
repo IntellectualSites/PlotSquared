43d71fe64be7ce71dbe02f2ea3cb09a24b08a941
Phillipp Glanz <p.glanz@madfix.me>
2023-04-05 13:09:33 +0200

    Add snapshot support for run paper

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,12 @@ project(":Core").name = "PlotSquared-Core"
 project(":Bukkit").name = "PlotSquared-Bukkit"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            name = "jmp repository"
+            url = uri("https://repo.jpenilla.xyz/snapshots")
+        }
+    }
+}
