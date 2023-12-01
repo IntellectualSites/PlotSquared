1a18adcd95bfd143ee76682e8c48d676180a44de
Alexander Brandes <mc.cache@web.de>
2023-12-02 00:17:28 +0100

    Declare explicit runtime dependency on 'junit-platform-launcher' (#4255)

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,7 @@ subprojects {
     dependencies {
         // Tests
         testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
     }
 
     plugins.withId("java") {
