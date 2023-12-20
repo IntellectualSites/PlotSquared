936f3a36c541ddcb045df772b27b5a6b29add6b0
dordsor21 <dordsor21@gmail.com>
2023-12-20 16:04:57 +0000

    Add since tags

--- Core/src/main/java/com/plotsquared/core/plot/PlotArea.java
+++ Core/src/main/java/com/plotsquared/core/plot/PlotArea.java
@@ -1454,6 +1454,8 @@ public abstract class PlotArea implements ComponentLike {
 
     /**
      * Get the maximum height that changes to plot components (wall filling, air, all etc.) may operate to
+     *
+     * @since TODO
      */
     public int getMaxComponentHeight() {
         return this.maxBuildHeight;
@@ -1461,6 +1463,8 @@ public abstract class PlotArea implements ComponentLike {
 
     /**
      * Get the minimum height that changes to plot components (wall filling, air, all etc.) may operate to
+     *
+     * @since TODO
      */
     public int getMinComponentHeight() {
         return this.minBuildHeight;
