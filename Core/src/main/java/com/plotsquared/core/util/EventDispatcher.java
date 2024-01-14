811e0ec38c1a278ab264aa036b173ad176b0a86f
Pierre Maurice Schwang <mail@pschwang.eu>
2024-01-14 14:17:16 +0100

    feat: events for plot buy process

--- Core/src/main/java/com/plotsquared/core/util/EventDispatcher.java
+++ Core/src/main/java/com/plotsquared/core/util/EventDispatcher.java
@@ -25,6 +25,7 @@ import com.plotsquared.core.configuration.Settings;
 import com.plotsquared.core.configuration.caption.TranslatableCaption;
 import com.plotsquared.core.events.PlayerAutoPlotEvent;
 import com.plotsquared.core.events.PlayerAutoPlotsChosenEvent;
+import com.plotsquared.core.events.PlayerBuyPlotEvent;
 import com.plotsquared.core.events.PlayerClaimPlotEvent;
 import com.plotsquared.core.events.PlayerEnterPlotEvent;
 import com.plotsquared.core.events.PlayerLeavePlotEvent;
@@ -49,6 +50,7 @@ import com.plotsquared.core.events.PlotUnlinkEvent;
 import com.plotsquared.core.events.RemoveRoadEntityEvent;
 import com.plotsquared.core.events.TeleportCause;
 import com.plotsquared.core.events.post.PostPlayerAutoPlotEvent;
+import com.plotsquared.core.events.post.PostPlayerBuyPlotEvent;
 import com.plotsquared.core.events.post.PostPlotChangeOwnerEvent;
 import com.plotsquared.core.events.post.PostPlotDeleteEvent;
 import com.plotsquared.core.events.post.PostPlotMergeEvent;
@@ -57,6 +59,7 @@ import com.plotsquared.core.listener.PlayerBlockEventType;
 import com.plotsquared.core.location.Direction;
 import com.plotsquared.core.location.Location;
 import com.plotsquared.core.permissions.Permission;
+import com.plotsquared.core.player.OfflinePlotPlayer;
 import com.plotsquared.core.player.PlotPlayer;
 import com.plotsquared.core.plot.Plot;
 import com.plotsquared.core.plot.PlotArea;
@@ -313,6 +316,17 @@ public class EventDispatcher {
         PlayerPlotLimitEvent event = new PlayerPlotLimitEvent(player, calculatedLimit);
         eventBus.post(event);
         return event;
+    }
+
+    public PlayerBuyPlotEvent callPlayerBuyPlot(PlotPlayer<?> player, Plot plot, double price) {
+        PlayerBuyPlotEvent event = new PlayerBuyPlotEvent(player, plot, price);
+        eventBus.post(event);
+        return event;
+    }
+
+    public void callPostPlayerBuyPlot(PlotPlayer<?> player, OfflinePlotPlayer previousOwner, Plot plot,
+                                      double price) {
+        eventBus.post(new PostPlayerBuyPlotEvent(player, previousOwner, plot, price));
     }
 
     public void doJoinTask(final PlotPlayer<?> player) {
