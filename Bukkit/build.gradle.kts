afb36d98c74f686ef2692ef0b6408263eca6f6d8
Alexander Brandes <mc.cache@web.de>
2024-09-15 14:22:31 +0200

    Read Adventure version from version catalogue
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- Bukkit/build.gradle.kts
+++ Bukkit/build.gradle.kts
@@ -104,7 +104,7 @@ tasks {
         opt.links("https://jd.papermc.io/paper/1.20/")
         opt.links("https://docs.enginehub.org/javadoc/com.sk89q.worldedit/worldedit-bukkit/" + libs.worldeditBukkit.get().versionConstraint.toString())
         opt.links("https://intellectualsites.github.io/plotsquared-javadocs/core/")
-        opt.links("https://jd.advntr.dev/api/4.14.0/")
+        opt.links("https://jd.advntr.dev/api/" + libs.adventureApi.get().versionConstraint.toString())
         opt.links("https://google.github.io/guice/api-docs/" + libs.guice.get().versionConstraint.toString() + "/javadoc/")
         opt.links("https://checkerframework.org/api/")
         opt.isLinkSource = true
