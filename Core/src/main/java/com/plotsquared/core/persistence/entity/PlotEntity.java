517a46a389a71bd512953bfbb85a05df47cfb079
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-09-04 12:09:32 +0200

    feat: add PlotEntity class for database representation of plots

---
+++ Core/src/main/java/com/plotsquared/core/persistence/entity/PlotEntity.java
@@ -0,0 +1,98 @@
+package com.plotsquared.core.persistence.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "plot")
+@NamedQueries({
+        @NamedQuery(name = "Plot.findByWorldAndId", query = "SELECT p FROM PlotEntity p WHERE p.world = :world AND p.plotIdX = :x AND p.plotIdZ = :z"),
+        @NamedQuery(name = "Plot.findByOwner", query = "SELECT p FROM PlotEntity p WHERE p.owner = :owner"),
+        @NamedQuery(name = "Plot.findByWorld", query = "SELECT p FROM PlotEntity p WHERE p.world = :world")
+})
+public class PlotEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "plot_id_x", nullable = false)
+    private int plotIdX;
+    @Column(name = "plot_id_z", nullable = false)
+    private int plotIdZ;
+    @Column(length = 40, nullable = false)
+    private String owner;
+    @Column(length = 45, nullable = false)
+    private String world;
+    @Column(name = "timestamp", insertable = false, updatable = false)
+    private java.sql.Timestamp timestamp;
+
+    @OneToOne(mappedBy = "plot", cascade = CascadeType.ALL, optional = true, fetch = FetchType.LAZY)
+    private PlotSettingsEntity settings;
+
+    public PlotEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPlotIdX() {
+        return plotIdX;
+    }
+
+    public void setPlotIdX(int plotIdX) {
+        this.plotIdX = plotIdX;
+    }
+
+    public int getPlotIdZ() {
+        return plotIdZ;
+    }
+
+    public void setPlotIdZ(int plotIdZ) {
+        this.plotIdZ = plotIdZ;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public void setWorld(String world) {
+        this.world = world;
+    }
+
+    public java.sql.Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(java.sql.Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public PlotSettingsEntity getSettings() {
+        return settings;
+    }
+
+    public void setSettings(PlotSettingsEntity settings) {
+        this.settings = settings;
+    }
+}
