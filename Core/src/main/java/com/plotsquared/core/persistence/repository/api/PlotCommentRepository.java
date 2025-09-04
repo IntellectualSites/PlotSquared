8b690fd633c77d189513c9de2fb2a17470d76acc
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-09-04 16:20:27 +0200

    feat: enhance PlotCommentRepository with additional methods for hashcode handling

--- Core/src/main/java/com/plotsquared/core/persistence/repository/api/PlotCommentRepository.java
+++ Core/src/main/java/com/plotsquared/core/persistence/repository/api/PlotCommentRepository.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface PlotCommentRepository {
     List<PlotCommentEntity> findByWorldAndInbox(String world, String inbox);
+    List<PlotCommentEntity> findByWorldHashAndInbox(String world, int hashcode, String inbox);
     void save(PlotCommentEntity entity);
-    void deleteOne(String world, int hashcode, String inbox, String sender);
+    void deleteOne(String world, int hashcode, String inbox, String sender, String comment);
     void clearInbox(String world, String inbox);
+    void clearInbox(String world, int hashcode, String inbox);
+    void deleteByWorldAndHash(String world, int hashcode);
 }
