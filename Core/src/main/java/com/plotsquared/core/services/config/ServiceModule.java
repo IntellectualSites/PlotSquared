e1f393e4f3aae26cc31a57fb8ef88131e15d4c3e
Phillipp Glanz <p.glanz@madfix.me>
2025-09-07 00:26:55 +0200

    feat: bind PlotService to PlotDefaultService in ServiceModule

--- Core/src/main/java/com/plotsquared/core/services/config/ServiceModule.java
+++ Core/src/main/java/com/plotsquared/core/services/config/ServiceModule.java
@@ -2,13 +2,16 @@ package com.plotsquared.core.services.config;
 
 import com.google.inject.AbstractModule;
 import com.plotsquared.core.services.api.PlayerMetaService;
+import com.plotsquared.core.services.api.PlotService;
 import com.plotsquared.core.services.impl.PlayerMetaDefaultService;
+import com.plotsquared.core.services.impl.PlotDefaultService;
 
 public class ServiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(PlayerMetaService.class).to(PlayerMetaDefaultService.class);
+        bind(PlotService.class).to(PlotDefaultService.class);
     }
 
 }
